701d0729ff23c13e7da6699efe561a5b8377ce3d
Muatasim Qazi <muatasimqazi@gmail.com>
2017-07-19 14:15:20 -0700

    sort posts based on time posted

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, redirect, render_template
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -12,19 +14,22 @@ class Blog(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
-    body = db.Column(db.String(10000))
+    body = db.Column(db.String(20000))
+    date = db.Column(db.DateTime)
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, date=None):
         self.title = title
         self.body = body
+        if date is None:
+            date = datetime.utcnow()
+        self.date = date
 
 @app.route('/newpost', methods=['GET', 'POST'])
 def create_newpost():
 
-    blog_title = ''
-    blog_body = ''
     error_title = ''
     error_body = ''
+
     if request.method == 'POST':
 
         blog_title = request.form['blog-title']
@@ -34,54 +39,37 @@ def create_newpost():
             error_title = "Please take a shorter title"
         else:
             blog_body = request.form['blog-body']
-
             if not blog_body:
-                error_body = 'wrong'
+                error_body = 'Please fill in the body'
 
-            elif len(blog_body) > 10000:
-                error_body = 'long'
+            elif len(blog_body) > 20000:
+                error_body = 'Your blog post exceeds the limit'
 
             else:
                 blog_post = Blog(blog_title, blog_body)
                 db.session.add(blog_post)
                 db.session.commit()
                 blog_id = blog_post.id;
-
-                return redirect("/blog?id=" + str(blog_id) +'&title=' + blog_title + '&body=' + blog_body)
+                return redirect("/blog?id=" + str(blog_id))
 
     return render_template('new-post.html', error_title=error_title, error_body=error_body)
 
-
-@app.route('/', methods=['GET', 'POST'])
-@app.route('/blog')
+@app.route('/blog', methods=['GET', 'POST'])
 def display_posts():
 
-    blog_post = Blog.query.all()
-
     blog_id = request.args.get('id')
+    if  blog_id:
 
-    if not blog_id:
-        return render_template('blog.html', blogs=blog_post)
+        blog_post = Blog.query.filter_by(id=blog_id).first()
+        return render_template('blog.html', blog=blog_post, blog_id=blog_id)
 
     else:
-        blog_title = request.args.get('title')
-        blog_body = request.args.get('body')
+        blog_posts = Blog.query.all()
+        return render_template('blog.html', blogs=blog_posts)
 
-        return render_template('blog.html', blog_id=blog_id, blog_title=blog_title, blog_body=blog_body)
-
-
-    # blog_post = db.session.query(Blog.title).first()
-    # blog_post = Blog.query.get(1)
-    # blog_title = blog_post.title;
-
-
-
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def index():
-
-    blog_posts = Blog.query.all()
-    return render_template('blog.html', posts=blog_posts)
-
+    return redirect('/blog')
 
 if __name__ == '__main__':
     app.run()
